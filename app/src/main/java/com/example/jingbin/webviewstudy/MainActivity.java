ade3a0026cb087c063f5dbbd7e03f5b2470d3524
yining2013 <65989906+yining2013@users.noreply.github.com>
2022-08-29 18:50:49 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/jingbin/webviewstudy/MainActivity.java
+++ app/src/main/java/com/example/jingbin/webviewstudy/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
